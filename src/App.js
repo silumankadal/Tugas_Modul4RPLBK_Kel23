9fe3eba1b791b60770fb8f555fa906c8dbc746f5
silumankadal <63130587+silumankadal@users.noreply.github.com>
2022-11-02 23:52:55 +0700

    tugas modul4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Counter from './learn-useState/Counter';
+import Effect from './learn-useEffect';
+import Context from './learn-useContext';
+import Reducer from './learn-useReducer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App-header">
+        <nav className='NavBar'>
+          <ul className='ul'>
+            <li className='li'>
+              <Link to="/">Use State</Link>
+            </li>
+            <li className='li'>
+              <Link to="/effect">Use Effect</Link>
+            </li>
+            <li className='li'>
+              <Link to="/context">Use Context</Link>
+            </li>
+            <li className='li'>
+              <Link to='/reducer'>Use Reducer</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/' exact element={<Counter />} />
+          <Route path='/effect' exact element={<Effect />} />
+          <Route path='/context' exact element={<Context />} />
+          <Route path='/reducer' exact element={<Reducer />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
